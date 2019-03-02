1bbf9ae059fdec68b3552b59d2ff70cd168efa61
Nate Sticco <nsticco@gmail.com>
2019-03-02 11:05:13 -0800

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,9 +2,9 @@ class mac_profiles (
     $path = '/Library/Desktop Pictures/El Capitan.jpg'
 )
 {
-    mac_profiles_handler::manage { 'com.grahamgilbert.config.desktop':
+    mac_profiles_handler::manage { 'com.puppetmacprofiles.config.desktop':
         ensure      => 'present',
-        file_source => template('mac_profiles/com.grahamgilbert.config.desktop.mobileconfig.erb'),
+        file_source => template('mac_profiles/com.puppetmacprofiles.config.desktop.mobileconfig.erb'),
         type        => 'template',
     }
 }
