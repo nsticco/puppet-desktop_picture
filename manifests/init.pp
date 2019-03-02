e21447d3b4e0a1b09aef4a7b6c0e3085d3d6bc7b
Nate Sticco <nsticco@gmail.com>
2019-03-02 12:40:16 -0800

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,11 +1,14 @@
 class mac_profiles (
-    $path = '/Library/Desktop Pictures/El Capitan.jpg',
-    $organization = 'Puppet Mac Profiles'
-)
+  $path = '/Library/Desktop Pictures/El Capitan.jpg',
+  $organization = 'Puppet Mac Profiles',
+  $desktop_manage = false
+) 
 {
+  if $desktop_manage {
     mac_profiles_handler::manage { 'com.puppetmacprofiles.desktop':
-        ensure      => 'present',
-        file_source => template('mac_profiles/com.puppetmacprofiles.desktop.mobileconfig.erb'),
-        type        => 'template',
+      ensure      => 'present',
+      file_source => template('mac_profiles/com.puppetmacprofiles.desktop.mobileconfig.erb'),
+      type        => 'template',
     }
+  }
 }
