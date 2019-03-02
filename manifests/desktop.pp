d568e0293a7299b915017631edef27c05b62143d
Nate Sticco <nsticco@gmail.com>
2019-03-02 14:27:16 -0800

    Update desktop.pp

--- manifests/desktop.pp
+++ manifests/desktop.pp
@@ -1,11 +1,12 @@
-class mac_profiles::desktop {
-  if $desktop_manage {
-    $desktop_top_uuid = fqdn_uuid('com.puppetmacprofiles.desktop')
-    $desktop_content_uuid = fqdn_uuid('com.puppetmacprofiles.desktop content')
-    mac_profiles_handler::manage { 'com.puppetmacprofiles.desktop':
-      ensure      => 'present',
-      file_source => template('mac_profiles/com.puppetmacprofiles.desktop.mobileconfig.erb'),
-      type        => 'template',
-    }
+class mac_profiles::desktop (
+  $path = '/Library/Desktop Pictures/Lake.jpg',
+  $organization = 'Puppet Mac Profiles'
+) {
+  $desktop_configuration_uuid = fqdn_uuid('com.puppetmacprofiles.desktop')
+  $desktop_content_uuid = fqdn_uuid('com.puppetmacprofiles.desktop content')
+  mac_profiles_handler::manage { 'com.puppetmacprofiles.desktop':
+    ensure      => 'present',
+    file_source => template('mac_profiles/com.puppetmacprofiles.desktop.mobileconfig.erb'),
+    type        => 'template',
   }
 }
