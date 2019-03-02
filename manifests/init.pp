06fcc579fda9dd2a43a2bf8317ce8b526318baeb
Nate Sticco <nsticco@gmail.com>
2019-03-02 13:44:16 -0800

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,7 +6,7 @@ class mac_profiles (
 {
   if $desktop_manage {
     $desktop_top_uuid = fqdn_uuid('com.puppetmacprofiles.desktop')
-    $desktop_payload_uuid = fqdn_uuid('com.puppetmacprofiles.desktop' + 'payload')
+    $desktop_content_uuid = fqdn_uuid('com.puppetmacprofiles.desktop' + 'content')
     mac_profiles_handler::manage { 'com.puppetmacprofiles.desktop':
       ensure      => 'present',
       file_source => template('mac_profiles/com.puppetmacprofiles.desktop.mobileconfig.erb'),
